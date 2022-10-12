7a6c88be18aac0c4eabf0f9fc41bcf239a996d19
Ahmed Habib <ahmedboboooooko@gmail.com>
2022-10-12 15:24:12 +0200

    Finished Product

---
+++ src/constants/movie.js
@@ -0,0 +1,5 @@
+export const API_URL =
+  "https://api.themoviedb.org/3/movie/popular?api_key=fe5bbc2e5f9aa05625550c1800cbb1f9";
+export const API_IMG = "https://image.tmdb.org/t/p/w500/";
+export const API_SEARCH =
+  "https://api.themoviedb.org/3/search/movie?api_key=fe5bbc2e5f9aa05625550c1800cbb1f9&query";
